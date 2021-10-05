ba2fe99a08a60d8e96f522048db05a502d40ad88
LucasZhan <1012999614@qq.com>
2021-10-05 17:41:16 +1030

    modify run script

--- DeepLabV3Plus-Pytorch/run.sh
+++ DeepLabV3Plus-Pytorch/run.sh
@@ -1,7 +1,7 @@
-ROOT=/home/xuzhan/Documents/VOC2012_DRS_dataset_copy
+ROOT=dataset
 MODEL=deeplabv3plus_resnet101 # deeplabv3plus_resnet101, deeplabv3_resnet101
-ITER=3000
-BATCH=16
+ITER=20000
+BATCH=32
 LR=0.05
 
 
@@ -9,5 +9,5 @@ LR=0.05
 CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} --model ${MODEL} --gpu_id 0,1 --amp --total_itrs ${ITER} --batch_size ${BATCH} --lr ${LR}  --crop_val
 
 
-## evalutation with crf
-#CUDA_VISIBLE_DEVICES=0,1 python eval.py --gpu_id 0,1 --data_root ${ROOT} --model ${MODEL}  --val_batch_size 16  --ckpt checkpoints/best_${MODEL}_voc_os16.pth  --crop_val
+# evalutation with crf
+CUDA_VISIBLE_DEVICES=0,1 python eval.py --gpu_id 0,1 --data_root ${ROOT} --model ${MODEL}  --val_batch_size 16  --ckpt checkpoints/best_${MODEL}_voc_os16.pth  --crop_val
