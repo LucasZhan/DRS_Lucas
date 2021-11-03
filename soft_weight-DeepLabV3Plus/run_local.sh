c6fd67ed00728749d322f4fabbcaa000e2d123ee
LucasZhan <1012999614@qq.com>
2021-11-03 14:32:34 +1030

    add run_local.sh

---
+++ soft_weight-DeepLabV3Plus/run_local.sh
@@ -0,0 +1,44 @@
+ROOT=../dataset
+MODEL=deeplabv3plus_resnet101 # deeplabv3plus_resnet101, deeplabv3_resnet101
+ITER=3200
+BATCH=12
+LR=0.06
+
+mkdir -p logs
+
+identifier='IRNet_full_seg'
+
+output_dir=results/seg_labels
+soft_weight_root=/home/xuzhan/Documents/IRNet_Lucas/data/refined_soft_weight29_10_2021
+seg_labels_root=/home/xuzhan/Documents/irn/data/cross_epoch_exp_5/sem_seg
+train_file_path=./datasets/data/infer.txt
+val_file_path=./datasets/data/infer.txt
+output_imgs_list_path=./datasets/data/infer.txt
+
+num_workers=8
+
+#output_dir=results/seg_labels
+#output_imgs_list_path=datasets/data/infer.txt
+#irn_mask_root=/home/xuzhan/Documents/irn/data/cross_epoch_exp_5/sem_seg
+#irn_imgs_name_path=datasets/data/infer.txt
+
+# training with 2 GPUs
+CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} \
+                                        --model ${MODEL} \
+                                        --gpu_id 0,1 \
+                                        --output_dir ${output_dir} \
+                                        --output_imgs_list_path ${output_imgs_list_path} \
+                                        --seg_labels_root ${seg_labels_root} \
+                                        --train_file_path ${train_file_path} \
+                                        --val_file_path ${val_file_path} \
+                                        --amp \
+                                        --total_itrs ${ITER} \
+                                        --batch_size ${BATCH} \
+                                        --lr ${LR}  \
+                                        --num_workers ${num_workers} \
+                                        --soft_weight_root ${soft_weight_root} \
+                                        --crop_val 2>&1 | tee logs/${identifier}.txt
+
+
+## evalutation with crf
+#CUDA_VISIBLE_DEVICES=0,1 python eval.py --gpu_id 0,1 --data_root ${ROOT} --model ${MODEL}  --val_batch_size 16  --ckpt checkpoints/best_${MODEL}_voc_os16.pth  --crop_val | tee logs/'eval'${identifier}.txt
