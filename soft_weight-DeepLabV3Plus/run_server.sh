309ca697bed89f5f46ed71de79b61d5c9dd3a029
LucasZhan <1012999614@qq.com>
2021-11-03 19:08:48 +1030

    modification for run 5 experiments

--- soft_weight-DeepLabV3Plus/run_server.sh
+++ soft_weight-DeepLabV3Plus/run_server.sh
@@ -1,43 +1,48 @@
 ROOT=../dataset
 MODEL=deeplabv3plus_resnet101 # deeplabv3plus_resnet101, deeplabv3_resnet101
-ITER=4000
+ITER=5000
 BATCH=20
-LR=0.06
+LR=0.05
 
 mkdir -p logs
 
-identifier='28_10_2021_IRNet_full_2_DLV3+_sw'
 
-output_dir=results/seg_labels
-soft_weight_root=/home/xzhan/soft_weight_6_10_2021
+soft_weight_root=/home/xzhan/data/full_soft_weights
 seg_labels_root=~/data/irn_full_result/sem_seg
-train_file_path=./datasets/data/infer.txt
+train_file_path=./datasets/data/train_aug.txt
 val_file_path=./datasets/data/infer.txt
-output_imgs_list_path=datasets/data/infer.txt
+output_imgs_list_path=./datasets/data/train_aug.txt
 
 num_workers=8
 
-#output_dir=results/seg_labels
-#output_imgs_list_path=datasets/data/infer.txt
-#irn_mask_root=/home/xuzhan/Documents/irn/data/cross_epoch_exp_5/sem_seg
-#irn_imgs_name_path=datasets/data/infer.txt
+for exp_no in 1 2 3 4 5
+do
+  identifier='IRNet_full_2_DLV3+_sw_exp'${exp_no}
+  output_dir='/home/xzhan/data/IRNet_full_seg_2_DLV3+_sw_exp'${exp_no}
+  mkdir -p ${output_dir}
+  # training with 2 GPUs
+  CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} \
+                                          --model ${MODEL} \
+                                          --gpu_id 0,1 \
+                                          --output_dir ${output_dir} \
+                                          --output_imgs_list_path ${output_imgs_list_path} \
+                                          --seg_labels_root ${seg_labels_root} \
+                                          --train_file_path ${train_file_path} \
+                                          --val_file_path ${val_file_path} \
+                                          --amp \
+                                          --total_itrs ${ITER} \
+                                          --batch_size ${BATCH} \
+                                          --lr ${LR}  \
+                                          --num_workers ${num_workers} \
+                                          --soft_weight_root ${soft_weight_root} \
+                                          --crop_val 2>&1 | tee logs/${identifier}_log.txt
 
-# training with 2 GPUs
-CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} \
-                                        --model ${MODEL} \
-                                        --gpu_id 0,1 \
-                                        --output_dir ${output_dir} \
-                                        --output_imgs_list_path ${output_imgs_list_path} \
-                                        --seg_labels_root ${seg_labels_root} \
-                                        --train_file_path ${train_file_path} \
-                                        --val_file_path ${val_file_path} \
-                                        --amp \
-                                        --total_itrs ${ITER} \
-                                        --batch_size ${BATCH} \
-                                        --lr ${LR}  \
-                                        --num_workers ${num_workers} \
-                                        --soft_weight_root ${soft_weight_root} \
-                                        --crop_val 2>&1 | tee logs/${identifier}.txt
+done
+
+
+
+
+
 
 
 ## evalutation with crf
